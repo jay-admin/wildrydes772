14f28b31dd38db0379ef48bc92dc03b1b7e77fe1
jay-admin <best.j1306@outlook.com>
2024-07-10 17:37:46 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_VuIpf8d1F', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'qdesokn66q6qc7jov95l47989', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
